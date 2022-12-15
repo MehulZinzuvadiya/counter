796dbeb8ca33ad2d52beb8fb50bfd161846fe502
MehulZinzuvadiya <mehulzinzuvadiya098@gmail.com>
2022-12-15 13:44:50 +0530

    15/12/2022

---
+++ lib/homePage.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int i = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("$i"),
+          centerTitle: true,
+          actions: [
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    i++;
+                  });
+                },
+                child: Icon(Icons.add)),
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    i--;
+                  });
+                },
+                child: Icon(Icons.remove)),
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    i = i * 2;
+                  });
+                },
+                child: Icon(Icons.star)),
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    i = i * 3;
+                  });
+                },
+                child: Icon(Icons.account_box)),
+            InkWell(
+                onTap: () {
+                  setState(() {
+                    i = i * 8;
+                  });
+                },
+                child: Icon(Icons.add_card)),
+          ],
+        ),
+        body: Center(
+          child: InkWell(
+            onTap: () {
+              setState(() {
+                i = 1;
+              });
+            },
+            child: Container(
+              height: 40,
+              width: 40,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+                // borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text("Reset"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:counter/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
+}
